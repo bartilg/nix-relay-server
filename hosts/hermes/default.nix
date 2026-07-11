83f2a0e95d10a0fcda164f5168635263d6a3d6d8
Bart Ilg <bartilg1215@gmail.com>
2026-07-11 14:17:42 -0700

    add hermes host and pin home manager version

---
+++ hosts/hermes/default.nix
@@ -0,0 +1,24 @@
+let
+  settings = import ./settings.nix;
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../profiles/relay-base.nix
+    ../../users/users.nix
+  ];
+
+  networking.hostName = settings.hostName;
+
+  services.homelabPihole.nebulaSync.enable = settings.piholeNebulaSyncLeader;
+
+  networking.firewall = {
+    trustedInterfaces = settings.firewall.trustedInterfaces;
+  };
+
+  system.autoUpgrade = {
+    flake = "github:bartilg/nix-relay-server#${settings.hostName}";
+    dates = settings.autoUpgradeDates;
+  };
+  system.stateVersion = "26.05";
+}
